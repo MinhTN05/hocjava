7ccdb5a7d0940426f9f8b22fb785793ab495a17b
MinhTN05 <trannhatminh460@gmail.com>
2024-06-05 13:57:08 +0700

    ahihi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập giá trị a: ");
+        int a = scanner.nextInt();
+        System.out.println("Nhập giá trị b: ");
+        int b = scanner.nextInt();
+        int c = Math.max(a, b);
+        int d = Math.min(a, b);
+        System.out.println("Max = " + c);
+        System.out.println("Min = " + d);
+        scanner.close();
     }
 }
---
+++ src/Tn.java
@@ -0,0 +1,2 @@
+public class Tn {
+}
